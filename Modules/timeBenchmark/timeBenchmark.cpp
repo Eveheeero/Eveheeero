e9449ea704dba3ed1dec90cdaff2eb3bf1e2d989
Eveheeero <xhve00000@naver.com>
2022-05-03 12:59:02 +0900

    +
    Type changed

--- Modules/timeBenchmark/timeBenchmark.cpp
+++ Modules/timeBenchmark/timeBenchmark.cpp
@@ -4,9 +4,9 @@ namespace tbmark
 {
     unsigned long long tbmark(const char *path, bool print)
     {
-        std::chrono::nanoseconds duration = timing_exec(path, print);
+        unsigned long long duration = timing_exec(path, print);
 
-        return duration.count();
+        return duration;
     }
 
     unsigned long long timing_exec(const char *path, bool print)
@@ -14,7 +14,7 @@ namespace tbmark
         std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
         run(path, print);
         std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
-        return end - start;
+        return (end - start).count();
     }
 
     int run(const char *path, bool print)
@@ -27,26 +27,26 @@ namespace tbmark
         std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
         run();
         std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
-        return end - start;
+        return (end - start).count();
     }
     unsigned long long Wrapper::tbmark(int loopCount, unsigned long long totalTime)
     {
-        std::chrono::nanoseconds duration = timing_exec();
+        unsigned long long duration = timing_exec();
         // 시간이 짧을경우 여러번 반복한다
-        if (duration.count() / 1000'000'000 < 1)
-            return duration.count();
+        if (duration / 1000'000'000 < 1)
+            return duration;
         else
         {
-            int count = 9 - log10(duration.count());
-            std::chrono::nanoseconds highest = duration;
+            int count = 9 - log10(duration);
+            unsigned long long highest = duration;
             int now = 0;
             while (now < count)
             {
-                std::chrono::nanoseconds value = timing_exec();
+                unsigned long long value = timing_exec();
                 if (highest < value)
                     highest = value;
             }
-            return highest.count();
+            return highest;
         }
     }
 
