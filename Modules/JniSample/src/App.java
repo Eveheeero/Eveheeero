f9df981748fab27e5f2c10ab3d17c8f90d49f0ad
Eveheeero <xhve00000@naver.com>
2022-11-02 21:44:58 +0900

    +

--- Modules/JniSample/src/App.java
+++ Modules/JniSample/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     static {
         // System.loadLibrary("test_library"); // Need Library Name without so or dll extension
-        System.load("/home/eveheeero/workspace/_Evh/_Evh/Eveheeero/Jobs/JniSample/src/test_library.so"); // With absolute path
+        System.load("/home/eveheeero/workspace/_Evh/_Evh/Eveheeero/Modules/JniSample/src/test_library.so"); // With absolute path
     }
 
     public static native int mul(int a, int b);
