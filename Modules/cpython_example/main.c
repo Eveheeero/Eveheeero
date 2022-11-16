1099c191e26b08e4f3bc430afb8ced09292134ec
Eveheeero <xhve00000@naver.com>
2022-11-16 11:55:43 +0900

    +

---
+++ Modules/cpython_example/main.c
@@ -0,0 +1,39 @@
+// With install python-dev package via apt or apk...
+// All structures in this data changes with python version
+// https://docs.python.org/3/extending/extending.html
+// https://docs.python.org/ko/dev/c-api/typeobj.html
+#define PY_SSIZE_T_CLEAN
+#include <python3.9/Python.h>
+
+static PyObject*
+PrintHello(PyObject* self, PyObject* _NoArg)
+{
+  // Argument help.
+  // https://docs.python.org/ko/3/c-api/arg.html
+  printf("Hello World!\n");
+  return Py_None;
+}
+
+// Define Module Methods
+static PyMethodDef MyModuleMethod[] = {
+  {"Helpme", (PyCFunction)PrintHello, METH_NOARGS, "Print Hello."}, // {Name, Function, ArgsRule, Doc}
+  {    NULL,                    NULL,           0,           NULL}  // Essential (Maybe?)
+};
+
+// Define Module Information
+static struct PyModuleDef MyModule = {
+  PyModuleDef_HEAD_INIT,  // Initial
+  "MyModule",             // Module Name
+  "MyModule Description", // Module Description
+  -1,                     // Module Size
+  MyModuleMethod          // Module Method
+};
+
+PyMODINIT_FUNC
+PyInit_MyModule(void)
+{
+  printf("MyModule Init\n");
+  // Create Module
+  return PyModule_Create(&MyModule);
+}
+
---
+++ Modules/cpython_example/run.py
@@ -0,0 +1,3 @@
+import MyModule
+
+MyModule.Helpme()
