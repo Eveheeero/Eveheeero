9cbae60ac2abb3da85adb9844be32c9010902a47
Eveheeero <xhve00000@naver.com>
2022-11-16 12:53:32 +0900

    +

--- Jobs/BlobDownloader/Program.cs
+++ Jobs/BlobDownloader/Program.cs
@@ -1,10 +1,25 @@
 ﻿namespace Evh
 {
-  public class BlobDownloaderRuntime
+  public class BlobDownloader
   {
+    private HttpClient client;
     public static void Main()
     {
       Console.WriteLine("Start!");
+      var downloader = new BlobDownloader();
+      downloader.Download("https://www.youtube.com/watch?v=QH2-TGUlwu4");
+    }
+
+    public BlobDownloader()
+    {
+      client = new HttpClient();
+    }
+
+    public Boolean Download(String url = "")
+    {
+      var response = client.GetAsync(url).Result;
+      Console.WriteLine(response);
+      return true;
     }
   }
 }
