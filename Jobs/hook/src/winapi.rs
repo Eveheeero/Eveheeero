519d0f4ca8a7bf6aae67fc25008d0a2d7d65f32c
Eveheeero <xhve00000@naver.com>
2022-12-14 03:58:18 +0000

    +

--- Jobs/hook/src/winapi.rs
+++ Jobs/hook/src/winapi.rs
@@ -1,35 +1,44 @@
-use winapi::um::winuser::{
-    DispatchMessageA, GetMessageA, SetWindowsHookExW, TranslateMessage, UnhookWindowsHookEx, MSG,
-    WH_KEYBOARD_LL,
-};
+#[cfg(target_os = "windows")]
+mod wrap {
+    use winapi::um::winuser::{
+        DispatchMessageA, GetMessageA, SetWindowsHookExW, TranslateMessage, UnhookWindowsHookEx,
+        MSG, WH_KEYBOARD_LL,
+    };
 
-unsafe extern "system" fn callback(code: i32, wparam: usize, lparam: isize) -> isize {
-    println!("nCode: {}, wParam: {}, lParam: {}", code, wparam, lparam);
-    0
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let handle;
-    unsafe {
-        // 로우레벨(LL)이 아니면 핸들링이 안됨, 등록은 됨
-        // MOUSE는 움직이는것마다 핸들링이 걸려 오류발생
-        handle = SetWindowsHookExW(WH_KEYBOARD_LL, Some(callback), std::ptr::null_mut(), 0);
-
-        // input으로하면 핸들링이 안됨 해당방법을 사용해야함
-        let mut msg: MSG = MSG {
-            hwnd: std::ptr::null_mut(),
-            message: 0,
-            wParam: 0,
-            lParam: 0,
-            time: 0,
-            pt: std::mem::zeroed(),
-        };
-        while GetMessageA(&mut msg as *mut MSG, std::ptr::null_mut(), 0, 0) == 1 {
-            TranslateMessage(&msg);
-            DispatchMessageA(&msg);
-        }
-
-        UnhookWindowsHookEx(handle);
+    unsafe extern "system" fn callback(code: i32, wparam: usize, lparam: isize) -> isize {
+        println!("nCode: {}, wParam: {}, lParam: {}", code, wparam, lparam);
+        0
     }
+
+    pub(super) fn main() -> Result<(), Box<dyn std::error::Error>> {
+        let handle;
+        unsafe {
+            // 로우레벨(LL)이 아니면 핸들링이 안됨, 등록은 됨
+            // MOUSE는 움직이는것마다 핸들링이 걸려 오류발생
+            handle = SetWindowsHookExW(WH_KEYBOARD_LL, Some(callback), std::ptr::null_mut(), 0);
+
+            // input으로하면 핸들링이 안됨 해당방법을 사용해야함
+            let mut msg: MSG = MSG {
+                hwnd: std::ptr::null_mut(),
+                message: 0,
+                wParam: 0,
+                lParam: 0,
+                time: 0,
+                pt: std::mem::zeroed(),
+            };
+            while GetMessageA(&mut msg as *mut MSG, std::ptr::null_mut(), 0, 0) == 1 {
+                TranslateMessage(&msg);
+                DispatchMessageA(&msg);
+            }
+
+            UnhookWindowsHookEx(handle);
+        }
+        Ok(())
+    }
+}
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    #[cfg(target_os = "windows")]
+    wrap::main()?;
+
     Ok(())
 }
--- Jobs/hook/src/windows.rs
+++ Jobs/hook/src/windows.rs
@@ -1,16 +1,29 @@
 use windows::Win32::{
     Foundation::{BOOL, HINSTANCE, HWND, LPARAM, LRESULT, WPARAM},
-    UI::WindowsAndMessaging::{
-        CallNextHookEx, DispatchMessageA, GetMessageA, SetWindowsHookExW, TranslateMessage,
-        UnhookWindowsHookEx, HOOKPROC, MSG, WH_KEYBOARD_LL,
+    UI::{
+        Input::KeyboardAndMouse::GetAsyncKeyState,
+        WindowsAndMessaging::{
+            CallNextHookEx, DispatchMessageA, GetMessageA, SetWindowsHookExW, TranslateMessage,
+            UnhookWindowsHookEx, HC_ACTION, HOOKPROC, MSG, WH_KEYBOARD_LL,
+        },
     },
 };
 
 unsafe extern "system" fn callback(code: i32, wparam: WPARAM, laparam: LPARAM) -> LRESULT {
-    println!(
-        "code: {}, wparam: {}, lparam: {}",
-        code, wparam.0, laparam.0
-    );
+    if code == HC_ACTION.try_into().unwrap() {
+        for now in std::iter::once(0x20)
+            .chain(std::iter::once(0x0D))
+            .chain(0x30..0x39)
+            .chain(0x41..0x5A)
+        {
+            let space_status = GetAsyncKeyState(now);
+            if space_status.leading_ones() == 0 && space_status.trailing_zeros() == 0 {
+                // 0x8000
+                println!("{} is pressed", now);
+                break;
+            }
+        }
+    }
     CallNextHookEx(None, code, wparam, laparam)
 }
 
