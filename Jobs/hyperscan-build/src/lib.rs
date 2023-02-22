f2d28b4a57ae51ee60cce9096f82ff871c6288ec
Eveheeero <xhve00000@naver.com>
2023-02-22 05:10:18 +0000

    +

--- Jobs/hyperscan-build/src/lib.rs
+++ Jobs/hyperscan-build/src/lib.rs
@@ -4,23 +4,73 @@ use std::{path::PathBuf, process::Command};
 use utils::*;
 
 pub fn build(basedir: impl Into<PathBuf>) -> Result<(), Box<dyn std::error::Error>> {
-    let basedir: PathBuf = basedir.into();
+    let basedir: PathBuf = Into::<PathBuf>::into(basedir).canonicalize()?;
+    let mut boost = basedir.join("boost");
+    let mut hyperscan = basedir.join("hyperscan");
 
-    let boost = download_boost(&basedir, Some("boost-1.81.0"));
-    let hyperscan = download_hyperscan(&basedir, None);
+    #[cfg(windows)]
+    let exec_ext = ".exe";
+    #[cfg(not(windows))]
+    let exec_ext = "";
+    #[cfg(windows)]
+    let script_ext = ".bat";
+    #[cfg(not(windows))]
+    let script_ext = ".sh";
 
-    Command::new("./bootstrap.sh")
-        .current_dir(&boost)
-        .status()
-        .expect("Failed to execute process");
-    Command::new("./b2")
-        .current_dir(&boost)
-        .status()
-        .expect("Failed to execute process");
+    if !basedir.join("boost").exists() {
+        boost = download_boost(&basedir, None);
+    }
+    if !basedir.join("hyperscan").exists() {
+        hyperscan = download_hyperscan(&basedir, None);
+    }
 
-    let mut cmk = cmake::Config::new(hyperscan);
-    cmk.env("-DBOOST_ROOT", boost.to_str().unwrap());
-    cmk.build_target("hs");
+    if !boost.join("b2").exists() {
+        Command::new(format!("./bootstrap{script_ext}"))
+            .args(["cflags=-fPIC", "cxxflags=-fPIC"])
+            .current_dir(&boost)
+            .status()
+            .expect("Boost bootstrap failed");
+    }
+
+    if !boost.join("bin.v2").exists() {
+        Command::new(format!("./b2{exec_ext}"))
+            .args([
+                "--build-type=complete",
+                "--layout=tagged",
+                "runtime-link=static",
+                "runtime-link=static",
+                "variant=release",
+                "threading=multi",
+            ])
+            .current_dir(&boost)
+            .status()
+            .expect("Boost build failed");
+    }
+
+    if !hyperscan.join("build").exists() {
+        std::fs::create_dir(hyperscan.join("build"))?;
+
+        Command::new("cmake")
+            .args([
+                "..",
+                format!("-DBOOST_ROOT={}", boost.to_str().unwrap()).as_str(),
+                "-DBUILD_STATIC_AND_SHARED=ON",
+                "-DCMAKE_BUILD_TYPE=Release",
+                "-DCMAKE_C_FLAGS=\"-fPIC\"",
+                "-DCMAKE_CXX_FLAGS=\"-fPIC\"",
+                "-DFAT_RUNTIME=off",
+                "-GNinja",
+            ])
+            .current_dir(&hyperscan.join("build"))
+            .status()
+            .expect("CMake generation failed");
+
+        Command::new("cmake")
+            .args(["--build", "."])
+            .current_dir(&hyperscan.join("build"))
+            .status()
+            .expect("CMake build failed");
+    }
 
     Ok(())
 }
@@ -36,7 +86,7 @@ pub fn download_boost(basedir: &PathBuf, version_: Option<&str>) -> PathBuf {
     // 1.81.0
     let version_without_prefix = version.trim_start_matches("boost-");
 
-    match download_url_tar_gz::<true, false>(basedir,
+    match download_url_tar_gz::<true, true>(basedir,
         "boost",
         format!("https://boostorg.jfrog.io/artifactory/main/release/{version_without_prefix}/source/{version}.tar.gz", version= version.replace("-", "_").replace(".", "_")).as_str(),
     ) {
@@ -45,7 +95,7 @@ pub fn download_boost(basedir: &PathBuf, version_: Option<&str>) -> PathBuf {
     }
 
     if let None = version_ {
-        match download_url_tar_gz::<true, false>(
+        match download_url_tar_gz::<false, true>(
             basedir,
             "boost",
             format!("https://github.com/boostorg/boost/release/lastest/{version}.tar.gz").as_str(),
